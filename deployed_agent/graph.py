bf3cd128b916c6b552fe81ceafbc9df68033c41d
Hizkia Febianto <Hizkia.Febianto@ibm.com>
2025-05-29 23:40:41 +0700

    Creating agent and config for deployment

---
+++ deployed_agent/graph.py
@@ -0,0 +1,18 @@
+# Initiating Langchain Chat Models
+from langchain.chat_models import init_chat_model
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+
+model = init_chat_model("gpt-4.1-mini", model_provider= "openai", stream=True)
+
+# Node to invoke an LLM
+def call_llm(state: MessagesState):
+    return {"messages": model.invoke(state['messages'])}
+
+# build the graph 
+graph = (
+    StateGraph(MessagesState)
+    .add_node("call_llm", call_llm)
+    .add_edge(START, "call_llm")
+    .compile(name = "simple agent")
+)
